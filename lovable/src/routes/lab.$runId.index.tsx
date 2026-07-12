67490e1a4b747c61cae668163d46260a35b345d9
ArmmyC <k.kamolpopv@gmail.com>
2026-07-13 03:09:20 +0700

    Removed lovable folder

--- lovable/src/routes/lab.$runId.index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/lab/$runId/")({
-  beforeLoad: ({ params }) => {
-    throw redirect({ to: "/lab/$runId/video", params: { runId: params.runId } });
-  },
-});
